c0db5c6e9d420bc2244a4472aa1fa76c4dd02eba
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-12-18 20:15:17 -0500

    feat: add routing for XML to PDF converter page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/xml-to-pdf-converter/xml-to-pdf-converter.component').then(
+        (m) => m.XmlToPdfConverterComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
