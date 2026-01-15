e215d6ce0162801893a7e1469460bceb90cf0272
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2026-01-15 16:57:46 -0500

    feat: update API URL to remove versioning in environment configurations

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.1.16:9100/api/v1/',
+  apiUrl: 'http://192.168.1.16:9100/api/',
   enableDebugTools: true,
   logLevel: 'debug'
 };
